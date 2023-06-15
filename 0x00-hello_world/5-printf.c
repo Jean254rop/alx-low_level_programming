76feff0414a47e640da62360de7e188838d5048f
roy <rop>
2023-06-15 09:13:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main -a program for task 5
+* Return: 0 (correct)
+*/
+int main(void)
+{
+prinff("with proper grammar, but the outcome is a piece of art, \n")
+return (0);
+}
