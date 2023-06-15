adea0f3e4c7cf36439de5becb787fd49f1013cd2
roy <rop>
2023-06-15 12:24:50 -0700

    task 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unist.h>
+/**
+* main - program for task 8
+* Return 1 (right)
+*/
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stout);
+return (1);
+}
