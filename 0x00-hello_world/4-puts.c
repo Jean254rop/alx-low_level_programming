8613433cbfb1ddf4912d4717ef7762da4bb77f9f
roy <rop>
2023-06-15 09:00:30 -0700

    task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - a program for task 4
+* Return: 0 (we are okay)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
