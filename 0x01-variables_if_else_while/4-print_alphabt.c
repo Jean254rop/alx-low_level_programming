790fd13fcf05c6812b5320eb6f3945a2da34d488
roy <rop>
2023-06-19 05:03:40 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 char ch = 'a';
-while (ch <= 'z') 
-{       
-if (ch != 'e' && ch != 'q') 
-{       
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
+{
 putchar(ch);
-}       
-ch++;   
-}       
+}
+ch++;
+}   
 putchar('\n');
 return (0);
 }
