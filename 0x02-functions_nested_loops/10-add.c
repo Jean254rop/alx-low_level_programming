36230b0d5b095fd22a9e6513fd25cc1915aae9a2
roy <rop>
2023-06-21 06:59:11 -0700

    task 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * program for task 10
+ * add - adds two values
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * program for task 9
+ * to print 9 time table starting 0
+ */
+void print_times_table(void)
+{
+int multiplier = 0;
+while (multiplier <= 10)
+{
+int multiplier = multiplier * 9;
+_putchar((9)x %d = %d '\n', multiplier, result);
+multiplier++;
+}
+}
+
