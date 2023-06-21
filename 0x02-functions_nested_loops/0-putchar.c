1d6030f9f5ae5043d8bb0872da4868545f73406c
roy <rop>
2023-06-21 01:37:08 -0700

    task 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,6 @@
-#include <unistd.h>
-/**
-*-putchar -writes the character c to stdout 
-@c: The character to print 
-*
-* Return: on success 1.
-* on error, -1 is returned, and errno is set appropriately 
-*/
-int -putchar(char c)
+#include "main.h"
+int main(void)
 {
-	return(write(1, &c, 1));
+_putchar('\n');
+return (0);
 }
