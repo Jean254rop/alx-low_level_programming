7e152aa4a19e86e2070511c0a08dd4c233610713
roy <rop>
2023-06-21 12:26:33 -0700

    task 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * A program for task 4 - checks for aplhabetic character
  * @c: checkee
- * Returns 1 if c is a letter, lowercase or uppercase 0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase 0 otherwise
 */
 int _isalpha(int c)
 {
