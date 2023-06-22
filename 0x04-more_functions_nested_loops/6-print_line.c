e8641d1701d10d8c3a8320c31f200fd4a757ff70
roy <rop>
2023-06-22 11:36:29 -0700

    task 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line
+ * return: 0
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+	_putchar('_');
+}
+_putchar('\n');
+}
