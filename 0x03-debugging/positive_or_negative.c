f0d2bd2d448747011199f802feb7efbfa733abd8
roy <rop>
2023-06-22 00:41:04 -0700

    task 0-main.c, main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Checks if a number is positive or negative or zero
+* Return: 0 if correct
+*/
+void positive_or_negative(int n)
+{
+if (n > 0)
+{
+printf ("is positive");
+}
+else if (n == 0)
+{
+printf("is zero");
+}
+else
+{
+printf("is negative");
+}
+return;
+}
