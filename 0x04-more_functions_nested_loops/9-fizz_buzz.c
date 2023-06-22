09c03ee5631c70a75543dbd55259f13c91da0031
roy <rop>
2023-06-22 12:23:31 -0700

    task 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include  <stdio.h>
+#include "main.h"
+/**
+ * fizz_buzz - for fiz buzz, print numbers 1-100 with a new line
+ *
+ *return 0
+ */
+int main(void)
+{
+	int i;
+		for (i = 1; i <= 100; i++)
+		{
+			if (i % 15 == 0)
+				printf("fizzbuzz");
+			else if (i % 3 == 0)
+				printf("fizz");
+			else
+				print('\n', i);
+			if (i < 100)
+				printf(" ");
+}
+printf('\n');
+return (0);
+}
