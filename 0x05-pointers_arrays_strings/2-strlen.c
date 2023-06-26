2e7d9399f19532f64a34dbb8376033fb9137a045
roy <rop>
2023-06-26 05:44:53 -0700

    task 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - return the length of string
+ * @s: to be checked
+ * return 0
+ */
+int _strlen(char *s)
+{
+	int string_length = 0;
+while (s[string_length])
+	string_length++;
+	return (string_length);
+}
