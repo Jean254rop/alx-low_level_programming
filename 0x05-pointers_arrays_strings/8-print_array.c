11329fb2425eb8f19a457a5ee6f227f1f0803185
roy <rop>
2023-06-28 12:18:00 -0700

    task 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_array - prints elements of an array of integers, folloewd by a new line
+ * print_array - prints elements of an array, folloewd by a new line
  * @a: array to be printed
  * @n: number of elements to print
  */
