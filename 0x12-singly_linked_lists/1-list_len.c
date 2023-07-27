2344eeca5bb1cdf85d8178d76e86a22ec8b55bb1
roy <rop>
2023-07-27 14:51:50 -0700

    if task 1 runs

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns the number of elements
+ * @h: the linked list
+ */
+size_t list_len(const list_t *h)
+{
+size_t elements = 0;
+while (h)
+{
+elements++;
+h = h->next;
+}
+return (elements);
+}
