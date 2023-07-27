3d805df4f9165a9d8bf1d6662efb4741ed01a48a
roy <rop>
2023-07-27 16:41:51 -0700

    task hare

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * main: checks code
+ * return: 0
+ */
+int main(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+return (0);
+}
